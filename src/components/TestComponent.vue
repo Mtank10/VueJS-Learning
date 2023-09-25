2b5417df93b43203996f206e05f300b1a8f637af
shivam raj <rajshivam691@gmail.com>
2023-09-25 17:46:36 +0530

    add button for clear text and convert uppercase functionality

--- src/components/TestComponent.vue
+++ src/components/TestComponent.vue
@@ -1,6 +1,11 @@
 <template>
     <div id="testing">
-         {{ name }} Testing
+         Hello! {{ name }} Testing
+    </div>
+    <div class="main">
+    <input type="text" v-model="name"><br><br>
+    <button v-on:click="greet">Clear</button><br><br>
+    <button v-on:click="capitalize">Convert UpperCase</button>
     </div>
 </template>
 
@@ -11,12 +16,24 @@
         return {
             name:"shivam raj"
         }
-    }
+    },
+    methods: {
+        greet:function(){
+        this.name=""
+      },
+      capitalize:function(){
+        this.name=this.name.toUpperCase()
+      },  
+    },
  }
 </script>
 <style>
  #testing{
     font-size: 4em;
+    text-align: center;
+ }
+ .main{
+    margin: 100px;
     text-align: center;
  }
 </style>
