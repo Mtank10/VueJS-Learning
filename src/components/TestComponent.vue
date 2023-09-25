d7faf526a5eb5535190c5f18dda3471ff974a5d4
shivam raj <rajshivam691@gmail.com>
2023-09-25 16:37:52 +0530

    add test component

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="testing">
+         {{ name }} Testing
+    </div>
+</template>
+
+<script>
+ export default {
+    name:"TestComponent",
+    data:()=>{
+        return {
+            name:"shivam raj"
+        }
+    }
+ }
+</script>
+<style>
+ #testing{
+    font-size: 4em;
+    text-align: center;
+ }
+</style>
+
